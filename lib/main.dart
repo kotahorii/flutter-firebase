44dc4af5dc7485adbe08257e431b6b7e6c5f35c2
kotahorii <khorii.micron@gmail.com>
2022-01-25 12:59:18 +0900

    [Update] auth page

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,96 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const ChatApp());
+Future<void> main() async {
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyCOdOh-UQAdTsnScQiCc_34pEu0knD892I",
+    appId: "1:976878824547:web:51eabe17283b32a7d5dc56",
+    messagingSenderId: "976878824547",
+    projectId: "login-test-7f41a",
+  ));
+  runApp(MyApp());
 }
 
-class ChatApp extends StatelessWidget {
-  const ChatApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ChatApp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyAuthPage(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
+class MyAuthPage extends StatefulWidget {
+  @override
+  _MyAuthPageState createState() => _MyAuthPageState();
+}
+
+class _MyAuthPageState extends State<MyAuthPage> {
+  String newUserEmail = '';
+  String newUserPassword = '';
+  String infoText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-                onPressed: () async {
-                  await Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => ChatPage()));
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
                 },
-                child: Text('ログイン'))
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ChatPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('チャット'),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () async {
-                await Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-              icon: Icon(Icons.close))
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => AddPostPage()));
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class AddPostPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('チャット投稿'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('戻る'),
-          onPressed: () => Navigator.of(context).pop(),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "パスワード (6文字以上)",
+                ),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final UserCredential result =
+                          await auth.createUserWithEmailAndPassword(
+                              email: newUserEmail, password: newUserPassword);
+                      final User? user = result.user;
+                      setState(() {
+                        infoText = "登録OK：${user?.email}";
+                      });
+                    } catch (e) {
+                      setState(() {
+                        infoText = "登録NG:${e.toString()}";
+                      });
+                    }
+                  },
+                  child: Text('ユーザー登録')),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(infoText)
+            ],
+          ),
         ),
       ),
     );
